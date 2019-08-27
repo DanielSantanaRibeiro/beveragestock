6cb43116adf65b77aa99c375f8fbcda5ba40d042
DanielSantanaRibeiro <danielsantanaribeiro2015@gmail.com>
2019-08-27 18:18:55 -0300

    First commit

---
+++ src/main/java/com/danielribeiro/beveragestock/BeveragestockApplication.java
@@ -0,0 +1,13 @@
+package com.danielribeiro.beveragestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeveragestockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeveragestockApplication.class, args);
+	}
+
+}
