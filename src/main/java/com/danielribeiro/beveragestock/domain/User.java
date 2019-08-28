1fb892196e5ea7de71d738020c91258fa5df91a1
DanielSantanaRibeiro <danielsantanaribeiro2015@gmail.com>
2019-08-27 23:42:52 -0300

    Creating USER domain class

---
+++ src/main/java/com/danielribeiro/beveragestock/domain/User.java
@@ -0,0 +1,71 @@
+package com.danielribeiro.beveragestock.domain;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private int ID;
+	private String name;
+	private String address;
+	private String phone;
+	
+	public User(int iD, String name, String address, String phone) {
+		super();
+		ID = iD;
+		this.name = name;
+		this.address = address;
+		this.phone = phone;
+	}
+	
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ID;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (ID != other.ID)
+			return false;
+		return true;
+	} 
+	
+	
+	
+}
