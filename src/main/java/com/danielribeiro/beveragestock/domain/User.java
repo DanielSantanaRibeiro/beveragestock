0859f4902c40248a607784d94306a133f5e3fb18
DanielSantanaRibeiro <danielsantanaribeiro2015@gmail.com>
2019-08-28 00:38:41 -0300

    Adding H2 DB and Spring Data JPA dependencies. Finishing USER domain class

--- src/main/java/com/danielribeiro/beveragestock/domain/User.java
+++ src/main/java/com/danielribeiro/beveragestock/domain/User.java
@@ -2,11 +2,19 @@ package com.danielribeiro.beveragestock.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
-	private int ID;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer ID;
 	private String name;
 	private String address;
 	private String phone;
